a1a92297464a0e54da08be46af11f07a3f7bb640
Niket16 <niketpatel1604@gmail.com>
2020-07-03 23:51:50 +0530

    swagger configuration,Cross policy con. added

--- src/main/java/com/reddit/redditclone/RedditCloneApplication.java
+++ src/main/java/com/reddit/redditclone/RedditCloneApplication.java
@@ -1,12 +1,16 @@
 package com.reddit.redditclone;
 
+import com.reddit.redditclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
